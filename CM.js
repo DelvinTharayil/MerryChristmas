d6989f95296479c6be12945d65b0fcd4faebab06
DelvinT <>
2020-12-23 21:13:22 +0530

    debug of animation

--- CM.js
+++ CM.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-    $("#Evpage1,#EvbackB,#Evopen,#EvbackT,#Evline,#Snow1,#Snow2,#Snow3,.pagefadeout,#Evstamp,#clicktext1,#Hbell,#Hstick,#Hsock,#Htree,#Hhat,#Hball,#Hgift").css({ animation: "null" })
+    $("#Evpage1,#EvbackB,#Evopen,#EvbackT,#Evline,.snowAll,.pagefadeout,#Evstamp,#clicktext1,.hover").css({ animation: "null" })
      $("#Reindeer,#Sman,#fadethings,#content,#bgletter,#Evpageup,#forestbg,#christmastextL,#wishes,#christmastext").css({ animation: "null" })
      $("#christmastext").fadeOut()
     $("body").click(function () {
-        $("#Evpage1,#EvbackB,#Evopen,#EvbackT,#Evline,#Snow1,#Snow2,#Snow3,.pagefadeout,#Evstamp,#clicktext1,#Hbell,#Hstick,#Hsock,#Htree,#Hhat,#Hball,#Hgift").css({ animation: "" })
+        $("#Evpage1,#EvbackB,#Evopen,#EvbackT,#Evline,.snowAll,.pagefadeout,#Evstamp,#clicktext1,.hover").css({ animation: "" })
         $("#Reindeer,#Sman,#fadethings,#content,#bgletter,#Evpageup,#forestbg,#christmastextL,#wishes,#christmastext").css({ animation: "" })
         $( "#christmastext" ).delay( 600).fadeIn(1000);
         $("#mainhover1").css({"transform":"translateY(-20px)"})
